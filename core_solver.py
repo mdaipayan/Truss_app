848393f9dd0ed3b315e47dac657ab317e7d4e04c
mdaipayan <49827916+mdaipayan@users.noreply.github.com>
2026-02-20 08:11:48 +0530

    Enhance TrussSystem with matrix storage
    Added storage for global and reduced stiffness matrices, and updated member class to store global stiffness matrix.

--- core_solver.py
+++ core_solver.py
@@ -3,12 +3,9 @@ import numpy as np
 class Node:
     def __init__(self, id, x, y, rx=0, ry=0):
         self.id = id
-        self.x = x
-        self.y = y
-        self.rx = rx  # 1 if restrained, 0 if free
-        self.ry = ry
-        self.ux = 0.0
-        self.uy = 0.0
+        self.x = x; self.y = y
+        self.rx = rx; self.ry = ry
+        self.ux = 0.0; self.uy = 0.0
 
 class Member:
     def __init__(self, id, node_i, node_j, E, A):
@@ -18,6 +15,7 @@ class Member:
         self.E = E
         self.A = A
         self.force = 0.0
+        self.k_global_matrix = None # NEW: Store element stiffness matrix
 
     @property
     def length(self):
@@ -33,6 +31,7 @@ class Member:
             [-c*c, -c*s, c*c, c*s],
             [-c*s, -s*s, c*s, s*s]
         ])
+        self.k_global_matrix = k_local # NEW: Save it for the UI
         return k_local
 
     def calculate_force(self):
@@ -48,7 +47,12 @@ class TrussSystem:
     def __init__(self):
         self.nodes = []
         self.members = []
-        self.loads = {} 
+        self.loads = {}
+        # NEW: Storage for Glass-Box visualization
+        self.K_global = None 
+        self.K_reduced = None
+        self.F_reduced = None
+        self.free_dofs = []
 
     def solve(self):
         n_dof = 2 * len(self.nodes)
@@ -65,12 +69,18 @@ class TrussSystem:
                 for j in range(4):
                     K_global[dofs[i], dofs[j]] += k[i, j]
 
-        # Solving for displacements
+        self.K_global = K_global # NEW: Store Global Matrix
+
         free_dofs = [2*n.id-2 for n in self.nodes if n.rx == 0] + [2*n.id-1 for n in self.nodes if n.ry == 0]
         free_dofs.sort()
+        self.free_dofs = free_dofs # NEW: Store partitioned indices
         
         K_reduced = K_global[np.ix_(free_dofs, free_dofs)]
         F_reduced = F_global[free_dofs]
+        
+        self.K_reduced = K_reduced # NEW: Store Reduced Matrix
+        self.F_reduced = F_reduced
+        
         U_reduced = np.linalg.solve(K_reduced, F_reduced)
         
         for i, dof in enumerate(free_dofs):
@@ -78,19 +88,15 @@ class TrussSystem:
             if dof % 2 == 0: self.nodes[node_idx].ux = U_reduced[i]
             else: self.nodes[node_idx].uy = U_reduced[i]
         
-       
-        # Calculate Reactions: R = K*U - F
-        
-       # Corrected Reaction Calculation
-        U_all = np.zeros(n_dof) # Use n_dof instead of n
+        U_all = np.zeros(n_dof) 
         for node in self.nodes:
             U_all[2*node.id-2] = node.ux
             U_all[2*node.id-1] = node.uy
         
-        # Use K_global and F_global instead of K and F
         Reactions = np.dot(K_global, U_all) - F_global 
         for node in self.nodes:
             node.rx_val = Reactions[2*node.id-2]
             node.ry_val = Reactions[2*node.id-1]
         
         return "Solved"
+        
